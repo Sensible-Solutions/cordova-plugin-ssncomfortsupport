530a724dffb0d9376c88f4ce538cc8f7926a00db
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2016-03-22 16:53:16 +0100

    Update ssncomfortsupport.js

--- www/ssncomfortsupport.js
+++ www/ssncomfortsupport.js
@@ -1,1 +1,35 @@
 
+var ssnComfortSupportName = "SsnComfortSupportPlugin";
+
+var ssncomfortsupport = {
+  openSettingsApp: function(successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, ssnComfortSupportName, "openSettingsApp", []); 
+	},
+	getWifiName: function(successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, ssnComfortSupportName, "getWifiName", []); 
+	}
+	/*startServer: function(successCallback, errorCallback, params) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "startServer", [params]); 
+	},
+	alarm: function(successCallback, errorCallback, params) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "alarm", []); 
+	},
+	registerNotifications: function(successCallback, errorCallback, params) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "registerNotifications", [params]); 
+	},
+	isBluetoothSharingAuthorized: function(successCallback) {
+		cordova.exec(successCallback, successCallback, gattServerName, "isBluetoothSharingAuthorized", []); 
+	},
+	getAlarmSettings: function(successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "getAlarmSettings", []); 
+	},
+	setAlarmSettings: function(successCallback, errorCallback, alerts, sound, vibration, log) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "setAlarmSettings", [{
+			"alerts": alerts,
+			"sound": sound,
+			"vibration": vibration,
+			"log":log
+		}]); 
+	}*/
+}
+module.exports = ssncomfortsupport;
