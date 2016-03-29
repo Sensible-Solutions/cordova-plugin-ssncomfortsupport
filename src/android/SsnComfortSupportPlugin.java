d40f66383c951717cdc11d431e57bf6008eafaf4
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2016-03-29 14:49:01 +0200

    Update SsnComfortSupportPlugin.java

--- src/android/SsnComfortSupportPlugin.java
+++ src/android/SsnComfortSupportPlugin.java
@@ -1,1 +1,30 @@
+/*
+* Copyright (C) 2016 Sensible Solutions Sweden AB
+*
+* Cordova plugin supporting the SenseSoft Notifications Comfort app.
+*/
+ 
+package com.sensiblesolutions.ssncomfortsupport;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+//import android.media.RingtoneManager;
+//import android.media.Ringtone;
+//import android.net.Uri;
+//import android.support.v4.app.NotificationCompat;
+//import android.app.NotificationManager;
+//import android.R;
+import android.app.AlertDialog;			        // For showing debug messaages
+import android.content.DialogInterface;		  // For showing debug messaages
+
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.UUID;
 
