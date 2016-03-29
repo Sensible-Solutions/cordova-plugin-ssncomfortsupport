9b166fdd7314b68b100f548da70cc609109eefa9
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2016-03-29 15:03:11 +0200

    Update SsnComfortSupportPlugin.java

--- src/android/SsnComfortSupportPlugin.java
+++ src/android/SsnComfortSupportPlugin.java
@@ -28,3 +28,34 @@ import android.content.DialogInterface;		  // For showing debug messaages
 //import java.util.List;
 //import java.util.UUID;
 
+public class SsnComfortSupportPlugin extends CordovaPlugin
+{
+ // General callback variables
+	//private CallbackContext serverRunningCallbackContext = null;
+	
+	// Action Name Strings
+	private final static String OPEN_SETTINGS_APP = "openSettingsApp";
+	private final static String GET_WIFI_NAME = "getWifiName";
+	
+	// Object keys
+	private final static String keyStatus = "status";
+	private final static String keyError = "error";
+	private final static String keyMessage = "message";
+	
+	// Status Types
+	private final static String statusOpenSettingsApp = "settingsApp";
+	private final static String statusGetWifiName = "wifiName";
+  
+	// Error Types
+	private final static String errorOpenSettingsApp = "settingsApp";
+	private final static String errorGetWifiName = "wifiName";
+	
+	// Error Messages
+ //private final static String logServerAlreadyRunning = "GATT server is already running";
+	//private final static String logService = "Immediate Alert service could not be added";
+	//private final static String logConnectionState = "Connection state changed with error";
+	//private final static String logStateUnsupported = "BLE is not supported by device";
+	//private final static String logStatePoweredOff = "BLE is turned off for device";
+	
+	
+}
