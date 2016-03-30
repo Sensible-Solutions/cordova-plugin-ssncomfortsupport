1806793926c2599a3c9f82bd15254284cf56c1ba
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2016-03-30 10:40:25 +0200

    Update SsnComfortSupportPlugin.m

--- src/ios/SsnComfortSupportPlugin.m
+++ src/ios/SsnComfortSupportPlugin.m
@@ -1,10 +1,48 @@
 /*
 * Copyright (C) 2016 Sensible Solutions Sweden AB
 *
-* Cordova plugin header for support to the SenseSoft Notifications Comfort app.
+* Cordova plugin supporting the SenseSoft Notifications Comfort app.
 */
 
-#import "GattServerPlugin.h"
+#import "SsnComfortSupportPlugin.h"
 
 //Plugin Name
-NSString *const pluginName = @"gattserverplugin";
+NSString *const pluginName = @"ssncomfortsupportplugin";
+
+
+@implementation SsnComfortSupportPlugin
+
+#pragma mark -
+#pragma mark Interface
+
+// Plugin actions
+- (void)openSettingsApp:(CDVInvokedUrlCommand *)command
+{
+
+}
+
+
+#pragma mark -
+#pragma mark Delegates
+
+// CBPeripheralManager Delegate Methods
+//
+
+
+#pragma mark -
+#pragma mark General helpers
+
+-(NSDictionary*) getArgsObject:(NSArray *)args
+{
+    if (args == nil)
+        return nil;
+    if (args.count != 1)
+        return nil;
+
+    NSObject* arg = [args objectAtIndex:0];
+
+    if (![arg isKindOfClass:[NSDictionary class]])
+        return nil;
+
+    return (NSDictionary *)[args objectAtIndex:0];
+}
