0dc277fac280f667768085335b3c16ce75a138f5
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2016-03-30 10:14:33 +0200

    Update SsnComfortSupportPlugin.h

--- src/ios/SsnComfortSupportPlugin.h
+++ src/ios/SsnComfortSupportPlugin.h
@@ -1,1 +1,21 @@
+/*
+* Copyright (C) 2016 Sensible Solutions Sweden AB
+*
+* Cordova plugin header for support to the SenseSoft Notifications Comfort app.
+*/
 
+#import <Cordova/CDV.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+//#import <AudioToolbox/AudioServices.h>
+
+@interface SsnComfortSupportPlugin : CDVPlugin
+{
+	
+	NSString *openSettingsAppCallback;
+	
+}
+
+- (void)openSettingsApp:(CDVInvokedUrlCommand *)command;
+- (void)getWifiName:(CDVInvokedUrlCommand *)command;
+
+@end
